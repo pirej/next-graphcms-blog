cb03adec5e17367e70bd8b5c375b196caaf8767d
Igor Risteski <pirej30@yahoo.com>
2022-01-05 03:34:47 +0100

    add hover animation

--- components/PostCard.js
+++ components/PostCard.js
@@ -9,6 +9,58 @@ const Wrapper = styled.div`
     width: 100%;
     margin-left: 2%;
   }
+
+  /* ----------------------- */
+  /* Sweep To Right */
+  .hvr-sweep-to-right {
+    border-radius: 8px;
+    display: inline-block;
+    vertical-align: middle;
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+    position: relative;
+    -webkit-transition-property: color;
+    transition-property: color;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    cursor: pointer;
+  }
+  .hvr-sweep-to-right:before {
+    border-radius: 8px;
+    content: "";
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: #103b61;
+    -webkit-transform: scaleX(0);
+    transform: scaleX(0);
+    -webkit-transform-origin: 0 50%;
+    transform-origin: 0 50%;
+    -webkit-transition-property: transform;
+    transition-property: transform;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -webkit-transition-timing-function: ease-out;
+    transition-timing-function: ease-out;
+  }
+  .hvr-sweep-to-right:hover,
+  .hvr-sweep-to-right:focus,
+  .hvr-sweep-to-right:active {
+    color: white;
+  }
+  .hvr-sweep-to-right:hover:before,
+  .hvr-sweep-to-right:focus:before,
+  .hvr-sweep-to-right:active:before {
+    -webkit-transform: scaleX(1);
+    transform: scaleX(1);
+  }
+
+  /* ----------------------- */
+
   .postWrap {
     display: flex;
     margin-bottom: 2%;
@@ -18,14 +70,14 @@ const Wrapper = styled.div`
       align-items: center;
     }
 
-    :hover {
+    /* :hover {
       background-color: #103b61;
       cursor: pointer;
-    }
-    :active {
+    }*/
+    /* :active {
       background-color: #266dad;
       cursor: pointer;
-    }
+    } */
     @media (max-width: 414px) {
       flex-direction: column;
     }
@@ -206,7 +258,7 @@ const PostCard = props => {
   const postContent = content[0].children[0].text;
   return (
     <Wrapper>
-      <div className="postWrap">
+      <div className="postWrap hvr-sweep-to-right">
         <div className="imgWrapper">
           <Image
             src={imgUrl}
