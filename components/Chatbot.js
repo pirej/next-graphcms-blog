9c2df6bfebb23ec4dfac2226a5eb3c086e3bd1b8
Igor Risteski <pirej30@yahoo.com>
2022-01-20 02:45:47 +0100

    CHATBOT - init logic setup

--- components/Chatbot.js
+++ components/Chatbot.js
@@ -1,38 +1,58 @@
 import Image from "next/image";
 import styled from "styled-components";
 import { useState } from "react";
-const minBot = "/bot-zzz.svg"; /* width={60} height={68} */
-const maxBot = "/bot-awake.svg"; /* width={189} height={166} */
+const minBot = "/bot-zzz.svg";
+const maxBot = "/bot-awake.svg";
 
 const StyledBotSection = styled.div`
   /* background-color: #aac9f2; */
 
   .btnWrapper {
-    display: flex;
-    justify-content: center;
+    position: fixed;
+    bottom: 85px;
+    right: 20%;
   }
 
   .botWrapper {
     position: fixed;
     bottom: 50px;
-    right: 4%;
+    right: 1%;
   }
 `;
 
 const Chatbot = () => {
   const [sleep, setSleep] = useState(true);
+  const [talking, setTalking] = useState(false);
+
+  if (!sleep && talking) {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+
+    recognition.onstart = function () {
+      console.log("Speech Recognition started");
+    };
+
+    recognition.onresult = function () {
+      console.log("eventot e ", event);
+    };
+    recognition.start();
+  }
+
   return (
     <StyledBotSection>
       {!sleep && (
         <div className="btnWrapper">
-          <button className="btn">Talk to me..</button>
+          <button className="btn" onClick={() => setTalking(true)}>
+            Talk to me
+          </button>
         </div>
       )}
       <div className="botWrapper" onClick={() => setSleep(!sleep)}>
         {sleep ? (
-          <Image src={minBot} width={189} height={70} alt="sleepingBot-image" />
+          <Image src={minBot} width={147} height={70} alt="sleepingBot-image" />
         ) : (
-          <Image src={maxBot} width={189} height={166} alt="awakenBot-image" />
+          <Image src={maxBot} width={147} height={166} alt="awakenBot-image" />
         )}
       </div>
     </StyledBotSection>
